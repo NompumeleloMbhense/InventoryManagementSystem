d563c6c589c794eaf76603019c8ba7a4a740c018
NompumeleloMbhense <nsmbhense6@gmail.com>
2026-01-26 21:09:11 +0200

    Add login and registration models, and implement login and registration pages

---
+++ ClientApp/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientApp.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
